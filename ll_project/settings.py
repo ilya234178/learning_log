43c91d0860685f7601e909c2f490266faae5fba9
Ilya2311 <veruhanov00@mail.ru>
2025-10-21 10:12:27 +0300

    corrections settings, models, admin

--- ll_project/settings.py
+++ ll_project/settings.py
@@ -31,6 +31,10 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    #Мои приложения
+    'learning_logs',
+    
+    #приложения по умолчанию
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
