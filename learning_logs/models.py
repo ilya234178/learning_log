b18ec6f16582b7ac86bd768e46a361b0e776582c
Ilya2311 <veruhanov00@mail.ru>
2025-10-22 10:15:47 +0300

    correct method __str__ in models

--- learning_logs/models.py
+++ learning_logs/models.py
@@ -21,4 +21,7 @@ class Entry(models.Model):
 
     def __str__(self):
         """Возвращает строковое представление модели."""
-        return f"{self.text[:50]}..."
+        if len(self.text) >= 50:
+            return f"{self.text[:50]}..."
+        else:
+            return self.text
